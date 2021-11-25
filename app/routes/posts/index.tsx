6abe44db915d9089e50edea42433f5a03532d79a
Nelo Puchades <mpuchades@verbio.com>
2021-11-25 20:23:23 +0100

    added loader to load posts

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,4 +1,25 @@
+import { useLoaderData } from 'remix';
+
+export let loader = () => {
+  return [
+    {
+      slug: 'my-first-post',
+      title: 'My first post',
+    },
+    {
+      slug: 'my-second-post',
+      title: 'My second post',
+    },
+    {
+      slug: 'my-third-post',
+      title: 'My third post',
+    },
+  ];
+};
+
 export default function Posts() {
+  let posts = useLoaderData();
+  console.log(posts);
   return (
     <div>
       <h1>Posts</h1>
